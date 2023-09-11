9495934c655f9f3ca052e415f60e55b3f820c31d
saumyasingh003 <saumyasingh98982@gmail.com>
2023-09-11 22:44:47 +0530

    commit

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const Register = () => {
+const Register = () => { 
   useEffect(() => {
     AOS.init();
     AOS.refresh();
